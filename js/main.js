a353a15849558ab76b60809b347433da1a840890
Jéssica Caetano <jessica.ocaetano@eaportal.org>
2025-08-30 23:35:23 +0000

    Estilição do Main parte do guia de conteúdo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const slider = new Siema({
+    loop: true
+})
+
+/*const buttonPrev = document.querySelector(".prev")*/
+const buttonNext = document.querySelector(".next")
+
+/*buttonPrev.addEventListener("click", () => slider.prev())*/
+buttonNext.addEventListener("click", () => slider.next())
