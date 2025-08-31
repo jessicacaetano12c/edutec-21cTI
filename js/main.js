ae60d5805760a457a4b107f7dff8c3a55fa4019b
Jéssica Caetano <jessica.ocaetano@eaportal.org>
2025-08-31 19:24:52 +0000

    Finalização da FAQ e estilização do Guia de conteúdo

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ const buttonNext = document.querySelector(".next")
 
 /*buttonPrev.addEventListener("click", () => slider.prev())*/
 buttonNext.addEventListener("click", () => slider.next())
+
+/*FAQ*/ 
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
