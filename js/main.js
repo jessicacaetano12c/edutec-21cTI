ab63cac01198ee63b78ee998d23b65a180f363f2
Jéssica Caetano <jessica.ocaetano@eaportal.org>
2025-09-02 22:02:21 +0000

    Estilização da div.conteudo

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const slider = new Siema({
     loop: true
 })
 
-/*const buttonPrev = document.querySelector(".prev")*/
+
 const buttonNext = document.querySelector(".next")
 
-/*buttonPrev.addEventListener("click", () => slider.prev())*/
+
 buttonNext.addEventListener("click", () => slider.next())
 
 /*FAQ*/ 
